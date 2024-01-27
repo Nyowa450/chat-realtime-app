7f84252779acb41c7f3f80f8fd462cbae2571fe0
Y.Ninomiya(kadoma) <kadoma.fukakusa248@gmail.com>
2024-01-27 19:02:42 +0900

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import '@/App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>hoge</div>
+    </>
+  )
+}
+
+export default App
---
+++ src/vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  root: 'src',
+  base: './',
+  // MEMO:この記述は<root>ディレクトリが省略されているので、すなわちsrc/publicを指す
+  publicDir: 'public',
+  server: {
+    port: 3000,
+    open: true,
+  },
+  resolve: {
+    // pathをプロジェクトルートからaliasで指定
+    alias: {
+      '@': `${__dirname}`,
+    },
+  },
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
+  plugins: [react()],
+});
